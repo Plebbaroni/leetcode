09c4b9b01a770255852a58158c2fe6f25620958d
Jared Lucas Amistad Schulz <gorillacyborgz@gmail.com>
2025-02-19 14:56:04 +1100

    🛣️62. Unique Paths

---
+++ dynamicprogramming/uniquePaths.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+
+        int[][] dp = new int[m][n];
+        Arrays.fill(dp[m-1], 1);
+        for (int i = m-2; i >= 0; i--) {
+            dp[i][n-1] = 1;
+            for (int j = n-2; j >= 0; j--) {
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+}
